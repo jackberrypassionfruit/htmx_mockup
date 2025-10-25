bf2675f33eaee0370df3b877362f2f7d7db62e07
jackberrypassionfruit <pashayanjack@gmail.com>
2025-10-25 09:45:24 -0700

    finished html tempalte

--- serialize/views.py
+++ serialize/views.py
@@ -2,9 +2,37 @@ from django.shortcuts import render
 
 # Create your views here.
 
+jobs = [
+    {
+        "job_number": "6238749",
+        "part_number": "9341028",
+        "qty_parts": 21,
+        "request_type": "Eng Trial",
+    },
+    {
+        "job_number": "6238746",
+        "part_number": "9341028",
+        "qty_parts": 21,
+        "request_type": "Eng Trial",
+    },
+    {
+        "job_number": "6238744",
+        "part_number": "9341028",
+        "qty_parts": 18,
+        "request_type": "Production",
+    },
+    {
+        "job_number": "6238743",
+        "part_number": "9341028",
+        "qty_parts": 19,
+        "request_type": "Production",
+    },
+]
+
 
 def index(request):
     return render(
         request,
         "base/index.html",
+        context={"jobs": jobs},
     )
