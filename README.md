c870255a3e45b86bdc0e4f52418e3e4744caa8c4
jackberrypassionfruit <pashayanjack@gmail.com>
2025-10-27 23:07:02 -0700

    used alpine.js for job selection + indications

---
+++ README.md
@@ -0,0 +1,4 @@
+django-livereload-server
+
+This django app adds a management command that starts a livereload server watching all your static files and templates as well as a custom runserver command that issues livereload requests when the development server is ready after a restart.
+Installation
--- serialize/views.py
+++ serialize/views.py
@@ -8,24 +8,28 @@ jobs = [
         "part_number": "9341028",
         "qty_parts": 21,
         "request_type": "Eng Trial",
+        "big": range(10000000000000),
     },
     {
         "job_number": "6238746",
         "part_number": "9341028",
         "qty_parts": 21,
         "request_type": "Eng Trial",
+        "big": range(10000000000000),
     },
     {
         "job_number": "6238744",
         "part_number": "9341028",
         "qty_parts": 18,
         "request_type": "Production",
+        "big": range(10000000000000),
     },
     {
         "job_number": "6238743",
         "part_number": "9341028",
         "qty_parts": 19,
         "request_type": "Production",
+        "big": range(10000000000000),
     },
 ]
 
