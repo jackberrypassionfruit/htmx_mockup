056bf11d31ead037584991ba74b2749836d4c13b
jackberrypassionfruit <pashayanjack@gmail.com>
2025-11-02 03:35:37 -0800

    got inline width styling to work

--- swimlane/views.py
+++ swimlane/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .etl.extract import get_scheduled_prints_df
 from .etl.transform import partition_prints_by_printer_ordered
+import polars as pl
 import os
 
 # Create your views here.
@@ -9,6 +10,18 @@ import os
 in_file_path = os.path.join(".", "swimlane", "test_files", "schedule_swimlane_now.csv")
 
 today_prints = get_scheduled_prints_df(in_file_path)
+
+today_prints = today_prints.with_columns(
+    pl.concat_str(
+        [
+            pl.lit("calc("),
+            pl.col("estimated_print_time_minutes") / 17.5,
+            pl.lit("% - 20px)"),
+        ],
+        separator="",
+    ).alias("x_coord_style")
+)
+
 prints_by_printer = partition_prints_by_printer_ordered(today_prints)
 
 
